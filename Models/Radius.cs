787ff1c923b4af4441b4653d46290c0ff28ee076
Afksenia <Afksenia@gmail.com>
2022-01-21 11:44:15 +0300

    Добавьте файлы проекта.

---
+++ Models/Radius.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp19.Models
+{
+    class Radius
+    {
+        public static double Add(double a) => 2 * 3.14 * a;
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp19.Models;
+
+namespace WpfApp19.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnotifyPropertyChanged([CallerMemberName] string PropertyName=null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnotifyPropertyChanged();
+            }
+        }
+        private int number2;
+        //public int Number2
+        //{
+        //    get => number2;
+        //    set
+        //    {
+        //        number2 = value;
+        //        OnotifyPropertyChanged();
+        //    }
+        //}
+        private double number3;
+        public double Number3
+        {
+            get => number3;
+            set
+            {
+                number3 = value;
+                OnotifyPropertyChanged();
+            }
+        }
+        // команда вместо обработчика события;
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Number3 = Radius.Add(Number1);
+        }
+        private bool CanAddCommandExecute(object p)
+        {
+            if (Number1!=0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecute);
+        }
+    }
+}
